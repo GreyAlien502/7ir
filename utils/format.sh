4cdabd1f314659bf5271a37c87088b24c59663b9
GreyAlien502 <greyalien502@gmail.com>
2018-03-21 23:29:25 -0400

    making sure scripts can run from any directory

--- utils/format.sh
+++ utils/format.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+format="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/format.sh
 if [ -d "$1" ]; then
 	find "$1" \
 		-type f \
 		-regex '.*\(oto\.ini\|prefix\.map\|\.ust\|\.txt\)' \
-		-exec "./$0" {} \;
+		-exec "$format" {} \;
 else
 	iconv -f shift-jis -t utf-8 "$1" > "$1".utf-8
 	mv -f "$1".utf-8 "$1"
--- utils/unzip-jis.sh
+++ utils/unzip-jis.sh
@@ -3,4 +3,5 @@ mkdir "$1".dir
 bsdtar -xf "$1" -C "$1".dir
 convmv -f shift-jis -t utf-8 -r "$1".dir --notest
 format="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/format.sh
-"$(format)" "$1".dir
+echo $format
+"$format" "$1".dir
